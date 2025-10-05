14a2e4a422c34ceae4faf2ead45de8bb8a8fae9d
scojr <180873402+scojr@users.noreply.github.com>
2025-10-04 20:26:27 -0500

    Add form inputs to Sidebar Panel component

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -8,14 +8,37 @@ export default function Sidebar() {
   return (
     <div className={'sidebar' + swapClass}>
       <header><button onClick={() => setSidesSwapped(!sidesSwapped)}></button></header>
+      <Panel data={'Name'}></Panel>
     </div >
   )
 }
 
-function Panel({ type }) {
+function Panel({ data }) {
   return (
     <div className="panel">
-      <h1>{type}</h1>
+      <h1>Element Properties</h1>
+      <div className="text-input-container">
+        <div className="separator"></div>
+        <label htmlFor="heading">Heading</label>
+        <input type="text" id='heading' value={'John'} />
+        <div className="separator"></div>
+        <label htmlFor="text-content">Text Content</label>
+        <textarea id="text-content" value={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus saepe vitae, beatae dolorum, perferendis quae unde minus consectetur cumque aliquid quaerat molestias! Minus, similique odio vel magni tenetur quam fugiat.'}></textarea>
+        <div className="separator"></div>
+      </div>
+      <div className="list-input-container">
+
+      </div>
+    </div>
+  )
+}
+
+function PanelEmpty() {
+  return (
+    <div className="panel empty">
+      <div className="box">
+        No Element Selected
+      </div>
     </div>
   )
 }
