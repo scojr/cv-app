baadf61186032ca142ceb25217fd002512e080e9
scojr <180873402+scojr@users.noreply.github.com>
2025-10-13 14:46:27 -0500

    Update placeholder-data module

--- src/placeholder-data.js
+++ src/placeholder-data.js
@@ -1,18 +1,45 @@
-const experience = [{
-  title: 'Front-End Developer',
-  company: 'Ipsum Solutions',
-  date: 'Jan 2023 - Present',
-  location: 'New York, NY',
-  roles: ['Developed and maintained client-facing applications using React and Tailwind CSS.', 'Improved site performance by 30% through optimized code and image handling.', 'Collaborated with designers and backend developers to launch projects on schedule.'],
-},
-{
-  title: 'Web Developer',
-  company: 'LoremTech',
-  date: 'Jan 2023 - Present',
-  location: 'New York, NY',
-  roles: ['Built over 20 responsive websites for small businesses and startups.',
-    'Integrated APIs for payment systems, booking platforms, and data visualization.',
-    'Provided ongoing client support, reducing reported issues by 40%.'],
-},]
+const placeholderSections = {
+  main: [
+    {
+      heading: 'Profile',
+      text: 'Results-driven web developer with 5+ years of experience designing and implementing responsive websites and applications. Skilled in modern front-end frameworks, backend systems, and UI/UX design. Adept at translating client needs into efficient digital solutions.',
+      list: [],
+    },
+    {
+      heading: 'Experience',
+      text: 'Ipsum Solutions 2030 - Present',
+      list: ['Developed and maintained client-facing applications using React and Tailwind CSS.', 'Improved site performance by 30% through optimized code and image handling.', 'Collaborated with designers and backend developers to launch projects on schedule.'],
+    },
+    {
+      heading: '',
+      text: 'LoremTech 2025 - 2029',
+      list: ['Built over 20 responsive websites for small businesses and startups.',
+        'Integrated APIs for payment systems, booking platforms, and data visualization.',
+        'Provided ongoing client support, reducing reported issues by 40%.'],
+    },
+  ],
+  aside: [
+    {
+      heading: 'Contact',
+      text: '',
+      list: ['123-555-1234', 'email@thiswebsite.com', 'www.theodinproject.com'],
+    },
+    {
+      heading: 'Skills',
+      text: '',
+      list: ['Project Management', 'Teamwork', 'Leadership', 'Effective Communication',],
+    },
+    {
+      heading: 'References',
+      text: 'Jaime Trout',
+      list: ['Phone: 123-456-7890', 'Email: email@thiswebsite.com'],
+    },
+    {
+      heading: '',
+      text: 'Bill Grayson',
+      list: ['Phone: 123-456-7890', 'Email: email@thiswebsite.com'],
+    },
+  ],
+}
 
-export { experience }
+export { placeholderSections };
